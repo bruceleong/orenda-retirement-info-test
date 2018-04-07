6d6906ccb35d2f1efc353e3cc81b95bcebc9c3de
devere-here <stevendeverehere@gmail.com>
2018-04-07 16:25:41 -0400

    feat: initialized firestore

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
+import {red500, yellow500, blue500} from 'material-ui/styles/colors';
+
+
 
 export default class Home extends Component {
     constructor(props) {
@@ -16,7 +20,10 @@ export default class Home extends Component {
         return (
             <div>
             <RaisedButton
-            label="Drawer Test"
+            backgroundColor="white"
+            label={
+                <FontIcon className="material-icons" color={red500}>flight_takeoff</FontIcon>
+            }
             onClick={this.handleToggle}
           />
           <Drawer open={this.state.open}>
