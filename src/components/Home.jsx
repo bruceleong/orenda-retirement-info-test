c1a55cea16c271782f854571ed8674e3ac0636a1
Bruce Leong <brucewleong@gmail.com>
2018-04-07 18:40:04 -0400

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,10 @@ import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
 import {red500, yellow500, blue500} from 'material-ui/styles/colors';
-
-
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import menu from './menu-alt-256.png';
 
 export default class Home extends Component {
     constructor(props) {
@@ -14,22 +16,22 @@ export default class Home extends Component {
         this.state = {open: false};
     }
 
-    handleToggle = () => this.setState({open: !this.state.open});
-
     render() {
         return (
             <div>
-            <RaisedButton
-            backgroundColor="white"
-            label={
-                <FontIcon className="material-icons" color={red500}>flight_takeoff</FontIcon>
-            }
-            onClick={this.handleToggle}
-          />
-          <Drawer open={this.state.open}>
-            <MenuItem>Menu Item</MenuItem>
-            <MenuItem>Menu Item 2</MenuItem>
-          </Drawer>
+            <div id='menu'>
+            <IconMenu
+            iconButtonElement={<IconButton><img src={menu}/></IconButton>}
+            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}
+          >
+          <Link to={'/About'}><MenuItem primaryText="About" /></Link>
+          <MenuItem primaryText="Send feedback" />
+          <MenuItem primaryText="Settings" />
+          <MenuItem primaryText="Help" />
+          <MenuItem primaryText="Sign out" />
+          </IconMenu>
+          </div>
                 <h1>Hey Participant! Welcome to Employee Resource</h1>
                 <p>where will i go</p>
                 <br />
@@ -45,3 +47,16 @@ export default class Home extends Component {
         )
     }
 }
+
+
+// <RaisedButton
+// backgroundColor="white"
+// label={
+//     <FontIcon className="material-icons" color={red500}>flight_takeoff</FontIcon>
+// }
+// onClick={this.handleToggle}
+// />
+// <Drawer open={this.state.open}>
+// <MenuItem>Menu Item</MenuItem>
+// <MenuItem>Menu Item 2</MenuItem>
+// </Drawer>
