a94084f7b8f73e0db46613eec50c4d29f88a0a57
Bruce Leong <brucewleong@gmail.com>
2018-04-07 22:34:49 -0400

    fix(form_news_contact_pages)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import dog from './RT.png'
 
 export default class About extends Component {
     constructor(props) {
@@ -11,11 +10,18 @@ export default class About extends Component {
     render() {
         return (
           <div>
-          <h1>SBSF is where your money goes to grow....</h1>
-          <h2>I love dogs</h2>
-          <img src={dog} />
+          <img src="https://sbsfinancials.com/wp-content/uploads/2017/03/cropped-Turtle-logo-MC-Final-with-tagline1200x527bluer-1.png" style={{width: '50vw', height: 'auto'}}/>
+          <h2>Your Retirement Planning Team</h2>
+          <h4>Ricky Tran</h4>
+          <p>Director of Payroll Processing</p>
+          <a href="mailto:rtran@sbsfinancials.com?Subject=Inquiry" target="_top">Contact</a>
           <br />
-          <Link to={'/'}>Home</Link>
+          <h4>Scott Tong</h4>
+          <p>Director of Compliance</p>
+          <a href="mailto:stong@sbsfinancials.com?Subject=Inquiry" target="_top">Contact</a>
+          <br />
+          <br />
+          <Link to={'/'}>Back Home</Link>
           </div>
         )
     }
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -5,16 +5,20 @@ import Principal from './Principal';
 import history from './history'
 import Transamerica from './Transamerica';
 import About from './About';
+import News from './News';
+import Forms from './Forms';
 
 export const Routes = () => {
   return (
     <Router history={history}>
         <Switch>
             {/* Routes placed here are available to all visitors */}
-            <Route exact path="/About" component={About} />
+            <Route exact path="/Contact" component={About} />
             <Route exact path="/" component={Home} />
             <Route exact path="/Principal" component={Principal} />
             <Route exact path="/Transamerica" component={Transamerica} />
+            <Route exact path="/News" component={News} />
+            <Route exact path="/Forms" component={Forms} />
         </Switch>
     </Router>
   )
