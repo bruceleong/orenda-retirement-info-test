5aa45b0f5ff0aff090f1d81d3340f9fc17e24085
Bruce Leong <brucewleong@gmail.com>
2018-04-09 14:55:53 -0400

    updatedAppDrawer

--- src/components/AppDrawer.js
+++ src/components/AppDrawer.js
@@ -20,23 +20,29 @@ export default class AppDrawer extends Component {
                 <Link to="/" style={{textDecoration: "none"}}>
                 <ListItem primaryText="Home" onClick={this.props.handleClose} />
                 </Link>
-                <Link to={"/Login"} style={{textDecoration: "none"}}>
-                <ListItem primaryText="Login" onClick={this.props.handleClose} />
-                </Link>
-                <Link to="/Contact" style={{textDecoration: "none"}}>
-                <ListItem primaryText="About" onClick={this.props.handleClose} />
-                </Link>
                 <Link to={"/Norms"} style={{textDecoration: "none"}}>
                 <ListItem primaryText="Norm's Restaurant (test)" onClick={this.props.handleClose} />
                 </Link>
                 <Link to={"/AudioInteriors"} style={{textDecoration: "none"}}>
                 <ListItem primaryText="Audio Interiors (test)" onClick={this.props.handleClose} />
                 </Link>
+                <a target='_blank' href='https://www.transamerica.com' style={{textDecoration: "none"}}>
+                <ListItem primaryText="401k Plan Details" onClick={this.props.handleClose} />
+                </a>
+                <a target='_blank' href='https://drive.google.com/open?id=17lT5REEVzmZPVSJrUVzyRS8rUh1KS9wt' style={{textDecoration: "none"}}>
+                <ListItem primaryText="Summary Plan Description" onClick={this.props.handleClose} />
+                </a>
                 <Link to="/News" style={{textDecoration: "none"}}>
                 <ListItem primaryText="News" onClick={this.props.handleClose} />
                 </Link>
                 <Link to="/Forms" style={{textDecoration: "none"}}>
                 <ListItem primaryText="Forms" onClick={this.props.handleClose} />
+                </Link>
+                <Link to={"/Login"} style={{textDecoration: "none"}}>
+                <ListItem primaryText="Login" onClick={this.props.handleClose} />
+                </Link>
+                <Link to="/Contact" style={{textDecoration: "none"}}>
+                <ListItem primaryText="About" onClick={this.props.handleClose} />
                 </Link>
                 <Link to="/Admin" style={{textDecoration: "none"}}>
                 <ListItem primaryText="Admin" onClick={this.props.handleClose} />
