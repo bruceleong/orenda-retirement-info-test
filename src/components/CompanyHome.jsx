e7435442bbbceb70c5010d98c8b65b0b62ddb365
Bruce Leong <brucewleong@gmail.com>
2018-04-18 13:03:31 -0400

    updatedCompanyHome

--- src/components/CompanyHome.jsx
+++ src/components/CompanyHome.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import { db } from '../config/constants'
 
 
@@ -14,48 +14,55 @@ class CompanyHome extends Component {
 
     componentDidMount() {
         let company = localStorage.getItem('company')
-        db.collection('companies').doc(company).collection('forms')
+        db.collection('companies').doc(company).collection('Forms')
             .get()
             .then(snapshot => {
                 let companyData = []
                 snapshot.forEach(doc => {
                     companyData.push(doc.data())
                 });
-                this.setState({companyData})
+                this.setState({ companyData })
             })
     }
 
     render() {
+        let result;
+        if (this.state.companyData) {
+            result = Object.keys(this.state.companyData[0]).map(data => ({ [data]: this.state.companyData[0][data] }))
+        }
+        console.log(result, 'results')
         console.log(this.props, 'props')
+        console.log(this.state, 'current state')
         return (
             <div>
-            {
-                !localStorage.getItem('company')
-                ? 'Wrong Page'
-                :
-                <div>
-                <h1>{localStorage.getItem('company')} Home</h1>
                 {
-                    !this.state.companyData
-                    ? ''
-                    :
-                    this.state.companyData.map(data => {
-                        return (
-                            <h1>
-                            {Object.keys(data)} -
-                            {Object.values(data)}
-                            </h1>
-                        )
-                    })
+                    !localStorage.getItem('company')
+                        ? 'Wrong Page'
+                        :
+                        <div>
+                            <h1>{localStorage.getItem('company')} Home</h1>
+                            {
+                                !this.state.companyData
+                                    ? ''
+                                    :
+                                    result.map(data => {
+                                        return (
+                                            <div key={Object.keys(data)}>
+                                            <a href={Object.values(data)}>
+                                            {Object.keys(data)}
+                                            </a>
+                                            </div>
+                                        )
+                                    })
+                            }
+                            <button onClick={() => {
+                                localStorage.clear()
+                                this.props.history.push(
+                                    '/'
+                                )
+                            }}>Logout</button>
+                        </div>
                 }
-                <button onClick={()=> {
-                    localStorage.clear()
-                    this.props.history.push(
-                        '/'
-                    )
-                }}>Logout</button>
-                </div>
-            }
             </div>
         )
     }
