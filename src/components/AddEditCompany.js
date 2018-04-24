c0b56e46fe3a1da8afea9886908ed7f790e92cb2
devere-here <stevendeverehere@gmail.com>
2018-04-24 02:03:18 -0400

    feat: reorganizing code behind forms

--- src/components/AddEditCompany.js
+++ src/components/AddEditCompany.js
@@ -100,41 +100,32 @@ export default class AddEditCompany extends Component {
                     <h3>Company Provider Website: {this.state.providerWebsite}</h3>
                     <a target="_blank" rel="noopener noreferrer" href={this.state.spd}><h3>Summary Plan Description:  {this.state.spd}</h3></a>
                     <form onSubmit={this.updateCompanyProfile}>
-                        <div>
-                            <label htmlFor="companyName">Company Name:</label><input
-                                name="companyName" value={this.state.companyName}
-                                required
-                                onChange={this.handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="companyProvider">Company Provider:</label><input
-                                name="companyProvider" value={this.state.companyProvider}
-                                required
-                                onChange={this.handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="providerWebsite">Company Provider Website:</label><input
-                                name="providerWebsite" value={this.state.providerWebsite}
-                                required
-                                onChange={this.handleChange} />
-                        </div>
-                        <div>
-                            <label htmlFor="companyProvider">Summary Plan Description:</label><input
-                                name="spd" value={this.state.spd}
-                                required
-                                onChange={this.handleChange} />
-                        </div>
-                        <button type="submit" onClick={() => { this.setState({ changesSubmitted: !this.state.changesSubmitted }) }}>Submit Changes</button>
-                        <br />
+                        <label style={{display: 'block', margin: '10px'}} htmlFor="companyName">Company Name:<input
+                            name="companyName" value={this.state.companyName}
+                            required
+                            onChange={this.handleChange} />
+                        </label>
+                        <label style={{display: 'block', margin: '10px'}} htmlFor="companyProvider">Company Provider:<input
+                            name="companyProvider" value={this.state.companyProvider}
+                            required
+                            onChange={this.handleChange} />
+                        </label> 
+                        <label style={{display: 'block', margin: '10px'}} htmlFor="providerWebsite">Company Provider Website:<input
+                            name="providerWebsite" value={this.state.providerWebsite}
+                            required
+                            onChange={this.handleChange} />
+                        </label>
+                        <label style={{display: 'block', margin: '10px'}} htmlFor="companyProvider">Summary Plan Description:<input
+                            name="spd" value={this.state.spd}
+                            required
+                            onChange={this.handleChange} />
+                        </label>
+                        <button type="submit" style={{display: 'block', margin: '0 auto'}} onClick={() => { this.setState({ changesSubmitted: !this.state.changesSubmitted }) }}>Submit Changes</button>
                         {
                             this.state.changesSubmitted &&
                             <div>
-                                <br />
-                                <button type="button" onClick={() => { this.setState({ changesSubmitted: !this.state.changesSubmitted }) }}>Click to make additional changes</button>
-                                <br />
-                                <h1>
-                                    Your changes were submitted
-                                </h1>
+                                <button style={{display: 'block', margin: '10px'}}type="button" onClick={() => { this.setState({ changesSubmitted: !this.state.changesSubmitted }) }}>Click to make additional changes</button>
+                                <h1>Your changes were submitted</h1>
                             </div>
                         }
                     </form>
