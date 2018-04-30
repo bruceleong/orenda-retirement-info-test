7795f4557e95da886d9a94407c8bc7027a461fb2
Steven DeVere <stevendeverehere@gmail.com>
2018-04-30 17:11:05 -0400

    Confirm card/ Add Company text/ reducing redux (#8)
    * feat: alert box to see if admin really wants to delete company
    
    * feat: changed the add company page text
    
    * chore: deleted forms from redux

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,10 @@ import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import company from './company'
-import forms from './forms'
 import allCompanies from './allCompanies'
 
 
-const reducer = combineReducers({company, allCompanies, forms})
+const reducer = combineReducers({company, allCompanies})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
@@ -17,4 +16,3 @@ const store = createStore(reducer, middleware)
 export default store
 export * from './company'
 export * from './allCompanies'
-export * from './forms'
