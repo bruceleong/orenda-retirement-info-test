195507d1fe4b3104971b7b33528eebcccac0e234
Bruce Leong <brucewleong@gmail.com>
2018-05-17 23:21:27 -0400

    added footer and report issue

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,10 @@ const App = () => (
       </div>
     <div id="footer">
       <p>© 2018 · Side By Side Financials · All Rights Reserved.</p>
-      <a style={{textDecoration: 'none', fontSize: '.75em'}} href="mailto:stevendeverehere@gmail.com,brucewleong@gmail.com?Subject=Inquiry" target="_top">Report a bug</a>
+      <a style={{textDecoration: 'none', fontSize: '.75em'}} href="mailto:stevendeverehere@gmail.com,brucewleong@gmail.com?Subject=Issue with SBSF" target="_top">Report an Issue</a>
       <br />
       <div style={{fontSize: '.75em'}}>
-      <p>Design by <a style={{textDecoration: 'none',color: 'black'}}href="https://www.bruceleong.com" target="_blank" rel="noopener noreferrer" >Leong</a> & <a style={{textDecoration: 'none', color: 'black'}} href="https://devere-here.github.io/portfolioPage/" target="_blank" rel="noopener noreferrer" >Devere</a> Designs</p>
+      <p>Design by <a style={{textDecoration: 'none', color: 'black'}}href="https://www.bruceleong.com" target="_blank" rel="noopener noreferrer" >Leong</a> & <a style={{textDecoration: 'none', color: 'black'}} href="https://devere-here.github.io/portfolioPage/" target="_blank" rel="noopener noreferrer" >Devere</a> Designs</p>
       </div>
     </div>
   </div>
