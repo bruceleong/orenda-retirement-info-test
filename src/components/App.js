abbd154154adf50f15a44a022315743a37092aad
Bruce Leong <brucewleong@gmail.com>
2018-06-11 16:29:35 -0400

    updated footer

--- src/components/App.js
+++ src/components/App.js
@@ -5,15 +5,15 @@ import Routes from './Routes'
 
 const App = () => (
   <div className="App">
-      <div>
-        <Routes />
-      </div>
+    <div>
+      <Routes />
+    </div>
     <div id="footer">
       <p>© 2018 · Side By Side Financials · All Rights Reserved.</p>
-      <a style={{textDecoration: 'none', fontSize: '.75em'}} href="mailto:stevendeverehere@gmail.com,brucewleong@gmail.com?Subject=Issue with SBSF" target="_top">Report an Issue</a>
+      <a style={{ textDecoration: 'none', fontSize: '1em' }} href="mailto:stevendeverehere@gmail.com,brucewleong@gmail.com?Subject=Issue with SBSF" target="_top">Report an Issue</a>
       <br />
-      <div style={{fontSize: '.75em'}}>
-      <p>Design by <a style={{textDecoration: 'none', color: 'black'}}href="https://www.bruceleong.com" target="_blank" rel="noopener noreferrer" >Leong</a> & <a style={{textDecoration: 'none', color: 'black'}} href="https://devere-here.github.io/portfolioPage/" target="_blank" rel="noopener noreferrer" >Devere</a> Designs</p>
+      <div style={{ fontSize: '.75em' }}>
+        {/*<p>Design by <a style={{textDecoration: 'none', color: 'black'}}href="https://www.bruceleong.com" target="_blank" rel="noopener noreferrer" >Leong</a> & <a style={{textDecoration: 'none', color: 'black'}} href="https://devere-here.github.io/portfolioPage/" target="_blank" rel="noopener noreferrer" >Devere</a> Designs</p>*/}
       </div>
     </div>
   </div>
