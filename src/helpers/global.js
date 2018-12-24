0e2b1359203b7c5d1a39e6b035ef1be7a39be20d
Bruce Leong <brucewleong@gmail.com>
2018-12-23 17:41:32 -0800

    fixed spelling

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -3,7 +3,7 @@ function tip() {
 }
 
 function planDocTip() {
-  alert("ATTENTION: \nYou will be redirected to Google Drive where you can view and print your company's Plan documents. Thank you!")
+  alert("ATTENTION: \nYou will be redirected to Google Drive where you can view and print your company's 'Plan Documents'. Thank you!")
 }
 
 export { tip, planDocTip };
