7c87d420470556fb035dfad75d973b7c9c2dba9c
Bruce Leong <brucewleong@gmail.com>
2019-04-06 16:20:44 -0700

    updated google drive language

--- src/helpers/global.js
+++ src/helpers/global.js
@@ -1,9 +1,9 @@
 function tip() {
-  alert("ATTENTION: \nTo fill out the form/s online in Google Drive - click 'Open with' and selected 'DocHub' at the top of the page. Alternatively you can download and print the forms. Thank you!")
+  // alert("ATTENTION: \nTo fill out the form/s online in Google Drive - click 'Open with' and selected 'DocHub' at the top of the page. Alternatively you can download and print the forms. Thank you!")
 }
 
 function planDocTip() {
-  alert("ATTENTION: \nYou will be redirected to Google Drive where you can view and print your company's 'Plan Documents'. Thank you!")
+  // alert("ATTENTION: \nYou will be redirected to Google Drive where you can view and print your company's 'Plan Documents'. Thank you!")
 }
 
 export { tip, planDocTip };
